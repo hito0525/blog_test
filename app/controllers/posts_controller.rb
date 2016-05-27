b7d940e746f90a08c9c3fdb585a71c4db209ce9d
hito0525 <hito.0525@gmail.com>
2016-05-27 20:24:15 +0900

    Newの開発

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,5 +5,23 @@ end
   def show
     @post = Post.find(params[:id])
   end
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to "/posts/#{@post.id}"
+  end
+
+private
+  def post_params
+    params.require(:post).permit(
+      :title,
+      :body,
+      :category
+      )
+
+  end
 
 end
